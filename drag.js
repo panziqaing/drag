7c16c3f4e3ad25b2228530d93b1b867a1f09974c
panziqiang <624053847@qq.com>
2017-07-03 20:15:17 +0800

    change3 drag.js demo1.html

--- drag.js
+++ drag.js
@@ -3,7 +3,9 @@
  */
 function drag(id) {
   var obj=document.getElementById('id');
-    obj.onmousedown=function () {
+     var disX=0;
+     var disY=0;
+     obj.onmousedown=function () {
         
     }
 }
